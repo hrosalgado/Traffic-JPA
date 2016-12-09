9d829c81744bd7f8d113314a4fafeec1a64c9cb6
mlnieves <martaverin93@gmail.com>
2016-12-10 00:49:24 +0100

    CompleteRoadsVM

---
+++ Traffic_SI_Project_Webapp/src/main/java/SI_ESEI/Traffic/webapp/RoadsVM.java
@@ -0,0 +1,75 @@
+package SI_ESEI.Traffic.webapp;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transaction;
+
+import org.zkoss.bind.annotation.BindingParam;
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.NotifyChange;
+
+import SI_ESEI.Traffic.Infraction;
+import SI_ESEI.Traffic.Road;
+import SI_ESEI.Traffic.TransactionUtils;
+import SI_ESEI.Traffic.webapp.util.DesktopEntityManagerManager;
+
+public class RoadsVM {
+
+	//Roads under edition
+	private Road currentRoad = null;
+	
+	public Road getcurrentRoad(){
+		return currentRoad;
+	}
+	
+	public List<Infraction> getInfractions(){
+		EntityManager em = DesktopEntityManagerManager.getDesktopEntityManager();
+		return em.createQuery("SELECT i FROM Infraction i", Infraction.class).getResultList();
+	}
+	
+	public List<Road> getRoads(){
+		EntityManager em = DesktopEntityManagerManager.getDesktopEntityManager();
+		return em.createQuery("SELECT r FROM Road r", Road.class).getResultList();
+	}
+	
+	@Command
+	@NotifyChange("roads")
+	public void delete(@BindingParam("r") Road road){
+		EntityManager em = DesktopEntityManagerManager.getDesktopEntityManager();
+		TransactionUtils.doTransaction(em, __ ->{
+			em.remove(road);
+		});
+	}
+	
+	@Command
+	@NotifyChange("currentRoad")
+	public void newRoad(){
+		this.currentRoad = new Road();
+	}
+	
+
+	@Command
+	@NotifyChange({"roads", "currentRoad"})
+	public void save(){
+		EntityManager em = DesktopEntityManagerManager.getDesktopEntityManager();
+		TransactionUtils.doTransaction(em, __ ->{
+			em.persist(this.currentRoad);
+		});
+		this.currentRoad = null;
+	}
+	
+
+	@Command
+	@NotifyChange("currentRoad")
+	public void cancel(){
+		this.currentRoad = null
+	}
+	
+
+	@Command
+	@NotifyChange("currentRoad")
+	public void edit(@BindingParam("r") Road road){
+		this.currentRoad = road;
+	}
+}
